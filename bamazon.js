4b35ec0c17d2957e7941c44f258ad9b6157fdb38
ronseide <ron.seide@gmail.com>
2019-04-17 15:53:38 -0400

    Fully Functional

--- bamazon.js
+++ bamazon.js
@@ -1,12 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-// console.log("This is mysql stuff ");
-// console.log(mysql);
-// console.log("This is inquirer stuff ")
-// console.log(inquirer);
 function start() {
-
     var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
@@ -14,71 +9,76 @@ function start() {
         password: "Dob120963",
         database: "bamazon"
     });
-
+    var boatBought = "";
     connection.connect(function (err) {
         if (err) throw err;
-        connection.query("SELECT * FROM products", 
-                        function (err, results, fields) {
-            if (err) throw err;
-            // console.log(result);
-            // console.log("Product ID   Product Name\n" + result[0].id + "           " + result[0].product_name);
-            for (var i = 0; i < results.length; i++) {
-                console.log(results[i]);
-
-                console.log("#" + (i+1) + ": " 
-                    + results[i].product_name 
-                    + " (" + results[i].department_name + ")" 
-                    + " - $" + results[i].price 
-                    + " (qty:" + results[i].stock_quantity + ")"
-                );
-            }
-                console.log(results[i]);
-            inquirer.prompt([
-            {
-                name: "choice",
-                type: "input",
-                message: "What do you want to buy?"
-            },
-            {
-                name: "quantity",
-                type: "input",
-                message: "How many of them?"
-            }])
-            .then(function(answer) {
-                console.log(results[answer.choice-1]);
-                console.log(results[answer.choice-1].price*answer.quantity);
-                if( results[answer.choice-1].quantity < answer.quantity ) {
-                    console.log("Sorry there's not enough");
-
-                }
-                else {
-                    console.log("Doing mysql to reduce number of items");
-                    connection.query(
-                        "UPDATE products SET stock_quantity WHERE ?",
-                        [
-                            {
-                                stock_quantity: stock_quantity - answer.quantity
-                            },
-                            {
-                                id: results[answer.choice-1].id
-                            }
-                        ],
-                        function(error) {
-                            if (error) throw err;
-                            console.log("Bought!");
-                        }
+        connection.query("SELECT * FROM products",
+            function (err, results, fields) {
+                if (err) throw err;
+                for (var i = 0; i < results.length; i++) {
+                    console.log("---------------------------------------------------")
+                    console.log("#" + (i + 1) + ": "
+                        + results[i].product_name
+                        + " (" + results[i].department_name + ")"
+                        + " - $" + results[i].price
+                        + " (qty:" + results[i].stock_quantity + ")"
                     );
+                    boatBought = results[i].product_name;
                 }
-
-                start();
-        
+                inquirer.prompt([
+                    {
+                        name: "choice",
+                        type: "input",
+                        message: "Select the ID number of the boat you want to buy"
+                    },
+                    {
+                        name: "quantity",
+                        type: "input",
+                        message: "How many boats do you want?"
+                    }])
+                    .then(function (answer) {
+                        console.log("You purchased a " + boatBought);
+                        console.log("Your total purchase price is $" + results[answer.choice - 1].price * answer.quantity);
+                        if (results[answer.choice - 1].quantity < answer.quantity) {
+                            console.log("Insufficient Quantity!");
+                        }
+                        else {
+                            connection.query(
+                                "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+                                [
+                                    answer.quantity,
+                                    answer.choice
+                                ],
+                                function (error) {
+                                    if (error) throw err;
+                                }
+                            );
+                        }
+                        // start();
+                        reStart();
+                    });
             });
-
-        });
     });
+    function reStart() {
+        inquirer
+        .prompt({
+          name: "reStart",
+          type: "list",
+          message: "Would you like to [Shop Again] or [Exit]?",
+          choices: ["Shop Again", "Exit"]
+        })
+        .then(function(answer) {
+          if (answer.reStart === "Shop Again") {
+            start();
+          }
+          else{
+            connection.end();
+          }
+        });
+      }
+      
 }
-
-
 start();
+
 
 
